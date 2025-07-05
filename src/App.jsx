6a8a2970b93b586e70395bfa1657e601c13de2fc
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-07-05 13:33:42 +0100

    added Search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header'
+import Search from './components/Search'
 
 function App() {
   return (
-    <main className='w-[90%] mx-auto flex flex-col pt-16'>
+    <main className='w-[90%] mx-auto flex flex-col gap-8 pt-16'>
       <Header />
+      <Search/>
     </main>
   )
 }
