e837afbb0d57e2cc168e01a9d6a5e8db8e8d3709
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-07-05 15:06:37 +0100

    open and close new note function implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { IoSunnyOutline } from "react-icons/io5";
 import { GoMoon } from "react-icons/go";
 import { IoIosAdd } from "react-icons/io";
-const Header = () => {
+const Header = ({openCreate}) => {
+    function handleNewNote() {
+        openCreate(true)
+    }
     return (
         <section className="flex flex-col gap-6">
             <h1 className="text-3xl font-bold">Note App</h1>
@@ -11,7 +14,7 @@ const Header = () => {
                     <input type="checkbox" defaultChecked className=" toggle toggle-neutral" />
                     <GoMoon className="text-xl" />
                 </section>
-                <button className="btn bg-black text-white border-black rounded-lg">
+                <button className="btn bg-black text-white border-black rounded-lg" onClick={handleNewNote}>
                     <IoIosAdd className="text-2xl" />
                     New Note
                 </button>
